9f4dcfb62858671e93fad4a52fef694e279d780b
haris.khan <haris.khan@TS-HARIS-PC>
2022-09-01 15:17:56 +0500

    Basic Sections Completed

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,35 @@
 const UpdateVideo = () => { 
     const vid = document.getElementById("vid");
     const source = document.getElementById("hvid");
+
+    const vid2 = document.getElementById("vid2");
+    const source2 = document.getElementById("hvid2");
+
+    const vid3 = document.getElementById("vid3");
+    const source3 = document.getElementById("hvid3");
+
+    const gif_img = document.getElementById("gif-img");
   
     if (window.matchMedia("(min-width: 1200px)").matches) {
       source.setAttribute("src", "https://www.typeform.com/static/home-page/hero/video-1920.mp4");
+      source2.setAttribute("src", "https://www.typeform.com/static/home-page/inline-form-1.mp4");
+      source3.setAttribute("src", "https://www.typeform.com/static/home-page/inline-form-2.mp4");
+      gif_img.setAttribute("src", "https://www.typeform.com/static/images/home-page/meaningful/meaningful-banner-lg.gif");
     } else if(window.matchMedia("(min-width: 777px)").matches){
       source.setAttribute("src", "https://www.typeform.com/static/home-page/hero/video-1024-below.mp4");
+      source2.setAttribute("src", "https://www.typeform.com/static/home-page/inline-form-1.mp4");
+      source3.setAttribute("src", "https://www.typeform.com/static/home-page/inline-form-2-mobile.mp4");
+      gif_img.setAttribute("src", "https://www.typeform.com/static/images/home-page/meaningful/meaningful-banner-lg.gif");
     }else if(window.matchMedia("(max-width: 776px)").matches){
       source.setAttribute("src", "https://www.typeform.com/static/home-page/hero/video-mobile.mp4");
+      source2.setAttribute("src", "https://www.typeform.com/static/home-page/inline-form-1-mobile.mp4");
+      source3.setAttribute("src", "https://www.typeform.com/static/home-page/inline-form-2-mobile.mp4");
+      gif_img.setAttribute("src", "https://www.typeform.com/static/images/home-page/meaningful/meaningful-banner-xs.gif");
     }
   
     vid.load();
+    vid2.load();
+    vid3.load();
 }
   
   
