dae43d868a8078217bd4d1300db5a853e8b3d6a4
haris.khan <haris.khan@TS-HARIS-PC>
2022-09-02 17:38:49 +0500

    Minor fixes

--- assets/main.js
+++ assets/main.js
@@ -199,4 +199,17 @@ const UpdateVideo = () => {
     CurrentSlide--;
     }
   });
+
+
+  window.onscroll = function() {scrollFunction()};
+ 
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      
+      document.getElementById("navbar").style.background = "white";
+    } else {
+     
+      document.getElementById("navbar").style.background = "none";
+    }
+  }
   
