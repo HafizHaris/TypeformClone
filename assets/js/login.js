db515673942afe8cf03dcfb2e00a055e59e0211d
haris.khan <haris.khan@TS-HARIS-PC>
2022-09-05 12:48:21 +0500

    login testing 3rd method

--- assets/js/login.js
+++ assets/js/login.js
@@ -23,3 +23,44 @@ LoginEmail.onfocusout = LoginEmail.onkeyup = function() {
 LoginPassword.onfocusout = LoginPassword.onkeyup = function() {
     ValidateField("login-password");
 };
+
+  // function to get response
+  function handleCredentialResponse(response) {
+    const responsePayload = decodeJwtResponse(response.credential);
+    alert(responsePayload.name);
+    // img.src = responsePayload.picture;
+    // id.innerHTML = responsePayload.sub;
+    // email.innerHTML = responsePayload.email;
+}
+
+window.onload = function () {
+    google.accounts.id.initialize({
+        // replace your client id below
+        client_id: "434226071651-hafcjpqmp07tt5rjebs6maq1nhl5rm60.apps.googleusercontent.com",
+        callback: handleCredentialResponse,
+        auto_select: true,
+        auto: true
+    });
+    google.accounts.id.renderButton(
+        document.getElementById("google-button"),
+        { theme: "filled_blue", size: "medium", width: '200' }  // customization attributes
+    );
+    // also display the One Tap dialog on right side
+    // important for auto login
+    // google.accounts.id.prompt(); 
+}
+
+// function to decode the response.credential
+function decodeJwtResponse(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    return JSON.parse(jsonPayload);
+}
+
+function signOut() {
+    google.accounts.id.disableAutoSelect();
+    location.reload();
+}
