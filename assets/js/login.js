442b84407c3d4ded95cc17e8d61f10bf0a5750a5
haris.khan <haris.khan@TS-HARIS-PC>
2022-09-05 11:12:53 +0500

    Login validation completed

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,13 +1,25 @@
-function ValidateField(FieldId){
-    const FieldElement = document.getElementById(Field).value;
-    if(Field.length == 0)
-        document.getElementById($`{Field}-error`).style.display = "block";
+const ValidateField = (FieldId) => {
+    const FieldValue = document.getElementById(FieldId).value;
+    if(FieldValue.length == 0){
+        document.getElementById(`${FieldId}-error`).style.display = "block";
+        return false;
+    }
+    document.getElementById(`${FieldId}-error`).style.display = "none";
+    return true;
 }
-
 function ValidateLoginForm(){
-    ValidateField("login-email");
-    ValidateField("login-password");
-    
+    if(ValidateField("login-email") && ValidateField("login-password"))
+        return true;
     return false;
 }
 
+// validate on typing and leaving input fields
+const LoginEmail = document.getElementById("login-email");
+const LoginPassword = document.getElementById("login-password");
+
+LoginEmail.onfocusout = LoginEmail.onkeyup = function() {
+    ValidateField("login-email");
+};
+LoginPassword.onfocusout = LoginPassword.onkeyup = function() {
+    ValidateField("login-password");
+};
