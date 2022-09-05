f0d3040c10620bc1adb80ac1e10dfa9536965ced
haris.khan <haris.khan@TS-HARIS-PC>
2022-09-05 12:42:12 +0500

    login test with google 3rd method

--- assets/js/login.js
+++ assets/js/login.js
@@ -23,27 +23,3 @@ LoginEmail.onfocusout = LoginEmail.onkeyup = function() {
 LoginPassword.onfocusout = LoginPassword.onkeyup = function() {
     ValidateField("login-password");
 };
-
-window.gapi.client
-        .init({
-          clientId:'434226071651-hafcjpqmp07tt5rjebs6maq1nhl5rm60.apps.googleusercontent.com',
-          scope: "haris.khan@techno-soft.com",
-          plugin_name:'Typeformclone'
-});
-
-// login with google
-function onSignIn(googleUser) {
-    // Useful data for your client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
-
-    // The ID token you need to pass to your backend:
-    var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
-  }
-
